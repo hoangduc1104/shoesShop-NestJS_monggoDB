bba4dff5e8573ec185bfd4bd0a184bea2895d6cf
hoangduc1104 <hoangduc91nda@gmail.com>
2023-05-18 11:13:54 +0700

    feat/Comment_realtime

--- src/socket/socket.gateway.ts
+++ src/socket/socket.gateway.ts
@@ -11,12 +11,42 @@ export class SocketGateway implements OnGatewayConnection {
   @WebSocketServer()
   server: Server;
 
-  handleConnection(client: Socket) {
-    console.log(`Client ${client.id} connected`);
+  // handleConnection(client: Socket) {
+  //   console.log(`Client ${client.id} connected`);
+  // }
+
+  users: Map<string, string> = new Map(); // Sử dụng Map để lưu thông tin người dùng và trạng thái typing
+
+  handleConnection(socket: Socket) {
+    this.users.set(socket.id, '');
+
+    this.server.emit('users', [...this.users.values()]);
+  }
+
+  handleDisconnect(socket: Socket) {
+    this.users.delete(socket.id);
+
+    this.server.emit('users', [...this.users.values()]);
   }
 
   @SubscribeMessage('newComment')
   async handleComment(client: Socket, comment: any) {
     this.server.emit('newComment', comment);
+  }
+
+  @SubscribeMessage('typing')
+  handleTyping(socket: Socket, username: string) {
+    this.users.set(socket.id, username);
+
+    this.server.emit('typing', username);
+  }
+
+  @SubscribeMessage('stopTyping')
+  handleStopTyping(socket: Socket) {
+    const username = this.users.get(socket.id);
+
+    this.users.set(socket.id, '');
+
+    this.server.emit('stopTyping', username);
   }
 }
